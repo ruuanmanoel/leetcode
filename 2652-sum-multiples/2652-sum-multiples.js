439f63001aa96d31575021b331aa6451bf38fd5a
Ruan Manoel <ruuanmanoel@gmail.com>
2024-01-22 01:38:58 -0300

    Time: 81 ms (14.47%), Space: 52.8 MB (5.03%) - LeetHub

---
+++ 2652-sum-multiples/2652-sum-multiples.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var sumOfMultiples = function(n) {
+    let soma=0
+    for(let i= 1;i <= n; i++ ){
+        if(i % 3 ==0 || i%5 == 0 || i%7==0)soma+=i;
+    }
+    return soma
+};
