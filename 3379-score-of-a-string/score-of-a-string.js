1666ea1b3ac79221ef3d80168e2b9449dbce5cad
Ruan Manoel <ruuanmanoel@gmail.com>
2024-09-12 18:44:49 -0300

    Time: 69 ms (19.87%) | Memory: 49.6 MB (84.05%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.js
+++ 3379-score-of-a-string/score-of-a-string.js
@@ -5,7 +5,14 @@
 var scoreOfString = function(s) {
     let variavel =0;
     for(let i = 0; i < s.length-1; i++){
-        variavel += Math.abs(s.charCodeAt(i) - s.charCodeAt(i+1)); 
+        if(s.charCodeAt(i) - s.charCodeAt(i+1) >= 0)
+        {
+            variavel += s.charCodeAt(i) - s.charCodeAt(i+1);
+        }
+        else{
+            variavel += (s.charCodeAt(i) - s.charCodeAt(i+1)) * -1;
+        }
+        
     }
     return variavel;
 };
